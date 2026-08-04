7cab42d519a9feb3a7b7cbc8ad5258425c083ee1
Jeronimo <jerogonzalez054@gmail.com>
2026-08-03 20:17:43 -0500

    jero: filtro y APICE

--- Jero/prueba_yolo.py
+++ Jero/prueba_yolo.py
@@ -1,6 +1,7 @@
 import cv2
 import math
 import time
+import numpy as np
 from ultralytics import YOLO
 
 model = YOLO("yolo11n.pt")
@@ -8,13 +9,22 @@ model = YOLO("yolo11n.pt")
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
+Kalman = cv2.KalmanFilter(4, 2)
+Kalman.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
+Kalman.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
+
+Kalman.processNoiseCov = np.eye(4, dtype=np.float32) * 0.03
 
 puntos_trayectoria = []
 posicion_anterior = None
 tiempo_anterior = time.time()
 velocidad = 0.0
-
+apice_x = None
+apice_y = float('inf')
+tiro_en_progreso = False
 PÍXELES_POR_METRO = 500
+
+
 
 while True:
     ret, frame = cap.read()
@@ -22,10 +32,12 @@ while True:
         break
     
     tiempo_actual = time.time()
+    prediccion = Kalman.predict()
+    pred_x, pred_y = int(prediccion[0][0]), int(prediccion[1][0])
     dt = tiempo_actual - tiempo_anterior
     tiempo_anterior =tiempo_actual
 
-    results = model.track(frame, device= 'cpu', verbose=False, classes=[32], conf=0.15, persist=True, imgsz=320)
+    results = model.track(frame, device= 'cpu', verbose=False, classes=[32], conf=0.08, persist=True, imgsz=480)
 
     centro_actual = None
 
@@ -38,6 +50,16 @@ while True:
         centro_y = int((y1 + y2) / 2)
         centro_actual = (centro_x, centro_y)
 
+        if centro_y < apice_y:
+            apice_y = centro_y
+            apice_x = centro_x
+            
+
+        medicion = np.array([[np.float32(centro_x)], [np.float32(centro_y)]])
+        Kalman.correct(medicion)
+
+        puntos_trayectoria.append((pred_x, pred_y))
+    
         if posicion_anterior is not None and dt > 0:
             distancia_pixeles = math.sqrt((centro_x - posicion_anterior[0])**2 + (centro_y - posicion_anterior[1])**2)
             distancia_metros = distancia_pixeles / PÍXELES_POR_METRO
@@ -45,11 +67,14 @@ while True:
             velocidad = velocidad_m_s * 3.6
 
         posicion_anterior = centro_actual
-    else: 
-        pass
 
-    puntos_trayectoria.append(centro_actual)
-    if len(puntos_trayectoria) > 15:
+        
+    else: 
+        puntos_trayectoria.append((pred_x, pred_y))
+        posicion_anterior = None
+
+    
+    if len(puntos_trayectoria) > 30:
         puntos_trayectoria.pop(0)
 
     annotated_frame = results[0].plot()
@@ -61,13 +86,25 @@ while True:
 
     cv2.putText(annotated_frame, f"Velocidad: {velocidad: .1f} km/h", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
+    if apice_x is not None and apice_y != float('inf'):
+        cv2.circle(annotated_frame, (apice_x, apice_y), 8, (0, 255, 255), -1)
+        cv2.putText(annotated_frame, "APICE", (apice_x - 20, apice_y -15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
+
+
+
 
 
 
     cv2.imshow("Proyecto YOLO - Balon + trayectoria + velocidad", annotated_frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
         break
+    elif key == ord('r'):
+        apice_x = None
+        apice_y = float('inf')
+        tiro_en_progreso = False
+
 
 cap.release()
 cv2.destroyAllWindows()
